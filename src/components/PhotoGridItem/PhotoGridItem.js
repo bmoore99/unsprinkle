1fd62eb0e1120569aec7b7bc77a281a2b5ef36e5
Brenna Moore <brenna.moore@vista.co>
2023-03-28 09:10:36 +1300

    exercise 2: improve images

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+  const baseSrc = src.split(".")[0];
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/avif"
+            srcset={`
+              ${baseSrc}.avif 1x,
+              ${baseSrc}@2x.avif 2x,
+              ${baseSrc}@3x.avif 3x
+          `}
+          />
+          <source
+            type="image/jpg"
+            srcset={`
+              ${baseSrc}.jpg 1x,
+              ${baseSrc}@2x.jpg 2x,
+              ${baseSrc}@3x.jpg 3x
+            `}
+          />
+          <Image alt="" src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -26,6 +45,7 @@ const Image = styled.img`
   display: block;
   width: 100%;
   height: 300px;
+  object-fit: cover;
   border-radius: 2px;
   margin-bottom: 8px;
 `;
